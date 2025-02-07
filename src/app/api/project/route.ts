1ea639c810c771d8da11156a9f1bbf2ff15249a3
Kamil Otawski <kamilotawski@gmail.com>
2025-02-07 23:30:21 +0100

    awwww7

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -6,12 +6,3 @@ export async function GET() {
   const projects = await prisma.project.findMany();
   return NextResponse.json(projects);
 }
-
-// POST a new user
-export async function POST(req: Request) {
-  const { name, city, imageUrl } = await req.json();
-  const project = await prisma.project.create({
-    data: { name, city, imageUrl },
-  });
-  return NextResponse.json(project, { status: 201 });
-}
