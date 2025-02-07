33cbea9f1ebdbe63c3cc6f5d3194be53485190ec
Kamil Otawski <kamilotawski@gmail.com>
2025-02-07 23:17:15 +0100

    awww

--- src/app/(content)/projects/[projectId]/page.tsx
+++ src/app/(content)/projects/[projectId]/page.tsx
@@ -6,23 +6,28 @@ interface ProjectPageProps {
 }
 
 export default async function ProjectPage({ params }: ProjectPageProps) {
-  const { projectId } = await params;
-  const project = await prisma.project.findUnique({
-    where: { id: projectId },
-  });
-  const cost = await prisma.cost.findUnique({
-    where: { projectId: projectId },
-  });
-  const parameters = await prisma.parameters.findUnique({
-    where: { projectId: projectId },
-  });
+  try {
+    const { projectId } = await params;
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+    });
+    const cost = await prisma.cost.findUnique({
+      where: { projectId: projectId },
+    });
+    const parameters = await prisma.parameters.findUnique({
+      where: { projectId: projectId },
+    });
 
-  console.log(project, cost, parameters);
+    console.log(project, cost, parameters);
 
-  return (
-    <div>
-      <h1>Project: {project?.name}</h1>
-      <ProjectDetails project={project} cost={cost} parameters={parameters} />
-    </div>
-  );
+    return (
+      <div>
+        <h1>Project: {project?.name}</h1>
+        <ProjectDetails project={project} cost={cost} parameters={parameters} />
+      </div>
+    );
+  } catch (e) {
+    console.error(e);
+    throw new Error("Project not found");
+  }
 }
