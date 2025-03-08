93e821c8f38ed97b5d41f669469b838a0fc312a0
Kamil Otawski <kamilotawski@gmail.com>
2025-03-08 17:32:07 +0100

    suspense added

--- src/app/(content)/add-project/page.tsx
+++ src/app/(content)/add-project/page.tsx
@@ -1,11 +1,16 @@
 import { createProject } from "@/actions/actions";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 import ProjectDataForm from "@/components/ProjectDataForm";
 
-import React from "react";
+import React, { Suspense } from "react";
 
 const AddProjectPage = () => {
-  return <ProjectDataForm action={createProject} />;
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <ProjectDataForm action={createProject} />;
+    </Suspense>
+  );
 };
 
 export default AddProjectPage;
