9d7ae9a810a81ed9ee01944dba0589a1d4728d34
Kamil Otawski <kamilotawski@gmail.com>
2025-03-16 15:02:15 +0100

    trying something new

--- src/app/(content)/add-project/page.tsx
+++ src/app/(content)/add-project/page.tsx
@@ -1,18 +1,20 @@
-import { createProject } from '@/actions/actions';
-import LoadingSpinner from '@/components/LoadingSpinner';
+import { createProject } from "@/actions/actions";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
-import ProjectDataForm from '@/components/ProjectDataForm';
-import { generateFlattenedDefaultObject } from '@/lib/customFunctions';
+import ProjectDataForm from "@/components/ProjectDataForm";
+import { generateFlattenedDefaultObject } from "@/lib/customFunctions";
 
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 
 const AddProjectPage = async () => {
   const costObject = await generateFlattenedDefaultObject([
-    'Project',
-    'Parameters',
-    'Cost',
+    "Project",
+    "Parameters",
+    "Cost",
   ]);
-  console.log(costObject);
+  costObject.image_url =
+    "https://res-console.cloudinary.com/duv2kieyz/thumbnails/v1/image/upload/v1740656853/bXktbmV4dGpzLXByb2plY3Qvc2cwNWNubTdsY3E5Y2N1Mmp5dmI=/preview";
+
   return (
     <Suspense fallback={<LoadingSpinner />}>
       <ProjectDataForm action={createProject} newInitialData={costObject} />;
