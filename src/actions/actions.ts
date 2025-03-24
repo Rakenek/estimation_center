f39d106c18836f0f1307ce61cb2a95858db297f8
Rakenek <kamilotawski@gmail.com>
2025-03-24 12:25:15 +0100

    small changes

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,70 +1,70 @@
-"use server";
+'use server';
 
-import { auth } from "@/auth";
-import { hashPasswordWithSalt } from "@/lib/bcryptFunctions";
-import { getPublicIdFromUrl } from "@/lib/customFunctions";
-import { PrismaClient, Role } from "@prisma/client";
-import { revalidatePath } from "next/cache";
-import { FullSteelPriceData, MaterialPriceData } from "@/lib/data";
+import { auth } from '@/auth';
+import { hashPasswordWithSalt } from '@/lib/bcryptFunctions';
+import { getPublicIdFromUrl } from '@/lib/customFunctions';
+import { PrismaClient, Role } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { FullSteelPriceData, MaterialPriceData } from '@/lib/data';
 
 const prisma = new PrismaClient(); // ✅ Use a single Prisma instance
 
 const requiredFields = [
-  "name",
-  "city",
-  "image_url",
-  "status",
-  "n03_do_PUM",
-  "powierzchnia_dzialki",
-  "powierzchnia_zabudowy_nadziemia",
-  "powierzchnia_zabudowy_podziemia",
-  "powierzchnia_zabudowy_nadziemia_poza_obrysem_podziemia",
-  "powierzchnia_niezabudowana_dzialki",
-  "powierzchnia_dachow",
-  "powierzchnia_elewacji",
-  "powierzchnia_netto",
-  "powierzchnia_netto_podziemia",
-  "powierzchnia_netto_nadziemia",
-  "pum_i_puu",
-  "pum",
-  "puu",
-  "powierzchnie_wspolne_nadziemia",
-  "powierzchnia_garazu_w_nadziemiu",
-  "liczba_kondygnacji_podziemnych",
-  "liczba_kondygnacji_nadziemnych",
-  "liczba_miejsc_parkingowych_w_budynku",
-  "liczba_parkliftow",
-  "ilosc_mieszkan",
-  "srednia_powierzchnia_mieszkania",
-  "udzial_powierzchni_wspolnych_nadziemia",
-  "pow_podziemia_do_pum_i_puu",
-  "n01",
-  "n03",
-  "roboty_ziemne",
-  "zabezpieczenie_wykopow",
-  "sciany_szczelinowe",
-  "roboty_palowe",
-  "prace_fundamentowe",
-  "konstrukcja_podziemia",
-  "konstrukcja_nadziemia",
-  "elewacje",
-  "dachy",
-  "wykonczenie_nadziemia",
-  "wykonczenie_podziemia",
-  "windy",
-  "parklifty",
-  "instalacje_klimatyzacyjne",
-  "instalacje_wodno_kanalizacyjne",
-  "instalacje_gazowe",
-  "instalacje_elektryczne",
-  "instalacje_teletechniczne",
-  "infrastruktura",
-  "dfa",
-  "zielen",
-  "sieci",
-  "koszty_budowy",
-  "bhp",
-  "offset_poza_dzialka",
+  'name',
+  'city',
+  'image_url',
+  'status',
+  'n03_do_PUM',
+  'powierzchnia_dzialki',
+  'powierzchnia_zabudowy_nadziemia',
+  'powierzchnia_zabudowy_podziemia',
+  'powierzchnia_zabudowy_nadziemia_poza_obrysem_podziemia',
+  'powierzchnia_niezabudowana_dzialki',
+  'powierzchnia_dachow',
+  'powierzchnia_elewacji',
+  'powierzchnia_netto',
+  'powierzchnia_netto_podziemia',
+  'powierzchnia_netto_nadziemia',
+  'pum_i_puu',
+  'pum',
+  'puu',
+  'powierzchnie_wspolne_nadziemia',
+  'powierzchnia_garazu_w_nadziemiu',
+  'liczba_kondygnacji_podziemnych',
+  'liczba_kondygnacji_nadziemnych',
+  'liczba_miejsc_parkingowych_w_budynku',
+  'liczba_parkliftow',
+  'ilosc_mieszkan',
+  'srednia_powierzchnia_mieszkania',
+  'udzial_powierzchni_wspolnych_nadziemia',
+  'pow_podziemia_do_pum_i_puu',
+  'n01',
+  'n03',
+  'roboty_ziemne',
+  'zabezpieczenie_wykopow',
+  'sciany_szczelinowe',
+  'roboty_palowe',
+  'prace_fundamentowe',
+  'konstrukcja_podziemia',
+  'konstrukcja_nadziemia',
+  'elewacje',
+  'dachy',
+  'wykonczenie_nadziemia',
+  'wykonczenie_podziemia',
+  'windy',
+  'parklifty',
+  'instalacje_klimatyzacyjne',
+  'instalacje_wodno_kanalizacyjne',
+  'instalacje_gazowe',
+  'instalacje_elektryczne',
+  'instalacje_teletechniczne',
+  'infrastruktura',
+  'dfa',
+  'zielen',
+  'sieci',
+  'koszty_budowy',
+  'bhp',
+  'offset_poza_dzialka',
 ];
 
 export async function createProject(
@@ -110,7 +110,7 @@ export async function createProject(
         image_url: formDataObject.image_url as string,
         status: formDataObject.status as string,
         n03_do_PUM: parseFloat(formDataObject.n03_do_PUM as string),
-        user_id: "4f5a47fc-51c7-40f4-8492-5405c9a374a9", // Replace with dynamic user_id if needed
+        user_id: '4f5a47fc-51c7-40f4-8492-5405c9a374a9', // Replace with dynamic user_id if needed
       },
     });
 
@@ -240,13 +240,13 @@ export async function createProject(
       },
     });
 
-    console.log("✅ Project created successfully");
-    revalidatePath("/search");
-    return { success: "Projekt z sukcesem utworzony" }; // ✅ Return success message
+    console.log('✅ Project created successfully');
+    revalidatePath('/search');
+    return { success: 'Projekt z sukcesem utworzony' }; // ✅ Return success message
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     }; // ✅ Handle errors gracefully
   }
 }
@@ -412,12 +412,12 @@ export async function updateProject(
         };
       }
     }
-    revalidatePath("/search");
-    return { success: "Projekt z sukcesem utworzony" };
+    revalidatePath('/search');
+    return { success: 'Projekt z sukcesem utworzony' };
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     }; // ✅ Handle errors gracefully
   }
 }
@@ -427,16 +427,16 @@ const handleDelete = async (publicId: string) => {
 
   try {
     const response = await fetch(`${baseUrl}/api/delete-image`, {
-      method: "DELETE",
+      method: 'DELETE',
       body: JSON.stringify({ publicId }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     const data = await response.json();
 
     console.log(data?.message);
   } catch (error) {
-    console.error("Error deleting image:", error);
+    console.error('Error deleting image:', error);
   }
 };
 
@@ -445,7 +445,7 @@ export async function deleteProject(
   formData: FormData
 ): Promise<{ errors?: { form: string }; success?: string }> {
   try {
-    const projectId = formData.get("projectId") as string;
+    const projectId = formData.get('projectId') as string;
     const selectedProject = await prisma.project.findUnique({
       where: { id: projectId },
     });
@@ -466,18 +466,18 @@ export async function deleteProject(
     });
     if (
       selectedProject.image_url !==
-      "https://res.cloudinary.com/duv2kieyz/image/upload/v1740656853/my-nextjs-project/sg05cnm7lcq9ccu2jyvb.jpg"
+      'https://res.cloudinary.com/duv2kieyz/image/upload/v1740656853/my-nextjs-project/sg05cnm7lcq9ccu2jyvb.jpg'
     ) {
       await handleDelete(getPublicIdFromUrl(selectedProject.image_url));
     }
 
-    console.log("✅ Project deleted successfully");
-    revalidatePath("/search");
-    return { success: "Projekt z sukcesem usunięty" };
+    console.log('✅ Project deleted successfully');
+    revalidatePath('/search');
+    return { success: 'Projekt z sukcesem usunięty' };
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     };
   }
 }
@@ -502,11 +502,11 @@ export async function createUserAction(
       },
     });
 
-    return { success: "Projekt z sukcesem utworzony" };
+    return { success: 'Projekt z sukcesem utworzony' };
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     };
   }
 }
@@ -526,11 +526,11 @@ export async function addSteelPriceData(
 
     // const user1 = await prisma.user.create({
     //   data: {
-    //     username: "Kamil",
-    //     email: "kam@gmail.com",
-    //     password: process.env.USER_PASS, // Normally you would hash the password
+    //     username: 'Kamil',
+    //     email: 'kam@gmail.com',
+    //     password: '', // Normally you would hash the password
     //     role: Role.ADMIN, // Assuming Role is an enum with values like USER, ADMIN, etc.
-    //     image: "",
+    //     image: '',
     //   },
     // });
 
@@ -554,11 +554,11 @@ export async function addSteelPriceData(
       },
     });
 
-    return { success: "Dane kosztu stali z sukcesem dodane" };
+    return { success: 'Dane kosztu stali z sukcesem dodane' };
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     };
   }
 }
@@ -607,11 +607,11 @@ export async function addMaterialPriceData(
       },
     });
 
-    return { success: "Dane kosztu stali z sukcesem dodane" };
+    return { success: 'Dane kosztu stali z sukcesem dodane' };
   } catch (error) {
-    console.error("❌ Database error:", error);
+    console.error('❌ Database error:', error);
     return {
-      errors: { form: "Coś poszło nie tak, spróbuj później" },
+      errors: { form: 'Coś poszło nie tak, spróbuj później' },
     };
   }
 }
