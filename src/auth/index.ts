edd8dc9936eab9854d1e4a127f3b8a4ad17c9021
Rakenek <kamilotawski@gmail.com>
2025-03-31 09:47:19 +0200

    removed signInPage

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,20 +1,20 @@
-import { comparePassword } from "@/lib/bcryptFunctions";
-import { prisma } from "@/lib/prisma";
-import NextAuth, { User, NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
+import { comparePassword } from '@/lib/bcryptFunctions';
+import { prisma } from '@/lib/prisma';
+import NextAuth, { User, NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
-export const BASE_PATH = "/api/auth";
+export const BASE_PATH = '/api/auth';
 
 const authOptions: NextAuthConfig = {
   providers: [
     Credentials({
-      name: "Credentials",
+      name: 'Credentials',
       credentials: {
         username: {
-          label: "Nazwa użytkownika",
-          type: "text",
+          label: 'Nazwa użytkownika',
+          type: 'text',
         },
-        password: { label: "Hasło", type: "password" },
+        password: { label: 'Hasło', type: 'password' },
       },
       async authorize(credentials): Promise<User | null> {
         try {
@@ -34,19 +34,19 @@ const authOptions: NextAuthConfig = {
           }
           return null;
         } catch (e) {
-          console.error("Error", e);
+          console.error('Error', e);
           return null;
         }
       },
     }),
   ],
   basePath: BASE_PATH,
-  pages: {
-    signIn: "/auth/signin", // 🔥 Custom login page
-  },
+  // pages: {
+  //   signIn: "/auth/signin", // 🔥 Custom login page
+  // },
   secret: process.env.NEXTAUTH_SECRET, // Secret is defined here for encryption
   session: {
-    strategy: "jwt", // Use JWT for session management
+    strategy: 'jwt', // Use JWT for session management
     maxAge: 5 * 24 * 60 * 60, // Session will last 30 days (in seconds)
   },
 };
